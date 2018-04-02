a8e3ad66a08ab3f6cf829655e178ac42031df702
Adam Phair <adamphair@Adams-MBP-2.woh.rr.com>
2018-04-02 13:31:53 -0400

    change to ex_2

--- ex_2/GasolineReceipt.java
+++ ex_2/GasolineReceipt.java
@@ -1,6 +1,12 @@
 public class GasolineReceipt{
   public static void main (String[] args) {
-    System.out.println ("+----------------------------+"); 
+    System.out.println ("+----------------------------+");
+    System.out.println ("+----------------------------+");
+    System.out.println ("+----------------------------+");
+    System.out.println ("+----------------------------+");
+    System.out.println ("+----------------------------+");
+    System.out.println ("+----------------------------+");
+    System.out.println ("+----------------------------+");
 
   }
 }
