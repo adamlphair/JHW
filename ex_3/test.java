e8e30f0e13c3109eedb31b89790c716de92159fa
Adam Phair <adamphair@Adams-MBP-2.woh.rr.com>
2018-04-02 13:37:51 -0400

    update to ex_3

---
+++ ex_3/test.java
@@ -0,0 +1,11 @@
+public class test {
+  public static void main(String [] args) {
+    System.out.println ("this is my first java prog....not really though");
+    System.out.println ("this is my first java prog....not really though");
+    System.out.println ("this is my first java prog....not really though");
+    System.out.println ("this is my first java prog....not really though");
+    System.out.println ("this is my first java prog....not really though");
+
+  }
+
+}
