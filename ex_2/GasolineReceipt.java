eb807c3cd7b5afc6fec3dfe7191b2b6fad03ad16
Adam Phair <adam.l.phair@gmail.com>
2018-04-04 11:44:37 -0400

    update to Gas receipt

--- ex_2/GasolineReceipt.java
+++ ex_2/GasolineReceipt.java
@@ -1,12 +1,17 @@
 public class GasolineReceipt{
   public static void main (String[] args) {
-    System.out.println ("+----------------------------+");
-    System.out.println ("+----------------------------+");
-    System.out.println ("+----------------------------+");
-    System.out.println ("+----------------------------+");
-    System.out.println ("+----------------------------+");
-    System.out.println ("+----------------------------+");
-    System.out.println ("+----------------------------+");
+    System.out.println ("+---------------------------------+");
+    System.out.println ("|                                 |");
+    System.out.println ("|           CORNER STORE          |");
+    System.out.println ("|                                 |");
+    System.out.println ("|   2018-04-04       11:39AM      |");
+    System.out.println ("|                                 |");
+    System.out.println ("|   Gallons:            15.870    |");
+    System.out.println ("|   Price/gallon       $ 2.349    |");
+    System.out.println ("|                                 |");
+    System.out.println ("|   Fuel total:        $37.28     |");  
+    System.out.println ("|                                 |");
+    System.out.println ("+---------------------------------+");
 
   }
 }
