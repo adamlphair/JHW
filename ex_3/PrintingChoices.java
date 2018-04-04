2d42770e6e088d45aec031f706fcd98b838489fb
Adam Phair <adam.l.phair@gmail.com>
2018-04-04 11:52:07 -0400

    add study drills

--- ex_3/PrintingChoices.java
+++ ex_3/PrintingChoices.java
@@ -2,6 +2,9 @@ public class PrintingChoices {
   public static void main(String [] args) {
       System.out.println ("Alpha");
       System.out.println ("Bravo");
+
+      System.out.println("Charlie");
+      System.out.println("Delta");
       System.out.println();
 
       System.out.println ("Echo");
@@ -13,7 +16,10 @@ public class PrintingChoices {
       System.out.println("India");
 
       System.out.println();
-      System.out.println("This" + " " + "is" + " " + "a" + " test."); 
+      System.out.println("This" + " " + "is" + " " + "a" + " test.");
+
+      //Extra Study Drills
+      System.out.println("I am" + " learning Java the" + " Hard Way!"); 
   }
 
 }
