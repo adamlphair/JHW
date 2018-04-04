c05d853b36f7564ce58cc7df21f6052274128288
Adam Phair <adam.l.phair@gmail.com>
2018-04-04 12:12:01 -0400

    add ex_4 and study drill

---
+++ ex_4/EscapeSequencesComments.java
@@ -0,0 +1,19 @@
+public class EscapeSequencesComments {
+  public  static void main (String[] args) {
+    //This exercise demonstrates escape sequences and comments
+    System.out.println("Learn\tJava\n\tthe\nHard\tWay\n\n");
+    System.out.println("\tLearn Java the \"Hard\" way!\n");
+    //System.out.frimp ("Learn java the Hard Way");
+    System.out.println("Hello\n"); //This line prints Hello.
+    System.out.println("Jello\by\n"); //This line prints Jelly.
+    /*The quick brown fox jumped over a lazy dog.
+      Quick wafting zephyrs vex bold Jim.*/
+    System.out /*Testing*/ .println("Hard to believe, eh?");
+    System.out.println("Surprised? /* abcde */ Or what did you expect?");
+    System.out.println("\\ // -=- \\ //");
+    System.out.println("\\\\ \\\\\\ \\\\\\\\"); //it takes 2 to make 1
+    System.out.println("I hope you understand \"escape sequences\" now.\n");
+    //and comments.
+  }
+
+}
---
+++ ex_4/extra.java
@@ -0,0 +1,22 @@
+public class extra {
+  public static void main(String [] args) {
+      System.out.println ("Alpha");
+      System.out.println ("Bravo\n");
+
+      System.out.println("Charlie");
+      System.out.println("Delta\n");
+
+      System.out.println ("Echo");
+      System.out.println ("Foxtrot\n");
+
+      System.out.println("Golf");
+      System.out.println("Hotel\n");
+      System.out.println("India\n");
+
+      System.out.println("This" + " " + "is" + " " + "a" + " test.");
+
+      //Extra Study Drills
+      System.out.println("I am" + " learning Java the" + " Hard Way!");
+  }
+
+}
