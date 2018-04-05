06734ae0ddd8bca7fd1e521ee91d325c2bc774ab
Adam Phair <adam.l.phair@gmail.com>
2018-04-05 10:29:44 -0400

    add ex_6, add some vars

---
+++ ex_6: Mathematical Operations/MathOperations.java
@@ -0,0 +1,14 @@
+public class MathOperations {
+  public static void main (String[] args) {
+    //Declar variables
+    int a, b, c, d, e, f, g;
+    double x, y, z;
+    String one, two, both;
+
+    a = 10;
+    b = 27;
+    System.out.println("a is " + a + ", b is " + b ); 
+
+  }
+
+}
