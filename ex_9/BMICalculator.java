e6c8b8a5c21f1216cc7092e0fe633e1405d6573b
Adam Phair <adam.l.phair@gmail.com>
2018-04-07 15:05:11 -0400

    update to BMI calc; converted metric to imperial

--- ex_9/BMICalculator.java
+++ ex_9/BMICalculator.java
@@ -4,17 +4,21 @@ public class BMICalculator {
   public static void main (String[] args ) {
 
     Scanner keyboard = new Scanner(System.in);
-    double m, kg, bmi;
+    double m, kg, bmi, bmi2, lbs, inches;
 
     System.out.println ("Your height in meters: ");
     m = keyboard.nextDouble();
-
+    inches = m * 0.0254;
     System.out.println("Your weight in kg: ");
     kg = keyboard.nextDouble();
+    lbs = kg * 0.453592;
 
     bmi = kg / (m * m);
+    bmi2 = (lbs / (inches * inches) * 703);
 
-    System.out.println ("Your BMI is " + bmi); 
+    // System.out.println ("Your BMI is " + bmi);
+
+    System.out.println ("Your BMI is " + bmi);
   }
 
 }
