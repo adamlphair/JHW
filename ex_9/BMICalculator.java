1625cf85da632e20b5b1783e65a9b42759e53597
Adam Phair <adam.l.phair@gmail.com>
2018-04-08 10:57:59 -0400

    added study drills

--- ex_9/BMICalculator.java
+++ ex_9/BMICalculator.java
@@ -4,20 +4,25 @@ public class BMICalculator {
   public static void main (String[] args ) {
 
     Scanner keyboard = new Scanner(System.in);
-    double m, kg, bmi, bmi2, lbs, inches;
+    double m, kg, bmi, bmi2, lbs, inches, totalHeight;
+    int feet;
 
-    System.out.println ("Your height in meters: ");
-    m = keyboard.nextDouble();
-    inches = m * 0.0254;
-    System.out.println("Your weight in kg: ");
-    kg = keyboard.nextDouble();
-    lbs = kg * 0.453592;
+    System.out.println ("Your height(feet only): ");
+    feet = keyboard.nextInt();
+    System.out.println("Your height(inches only): ");
+    inches = keyboard.nextDouble();
+    System.out.println("Your weight in lbs: ");
+    lbs = keyboard.nextDouble();
 
+    //convert imperial to metric
+    totalHeight = (feet * 12) + inches;
+
+    m = totalHeight * .0254;
+    kg = lbs * 0.453592;
+    
     bmi = kg / (m * m);
-    bmi2 = (lbs / (inches * inches) * 703);
 
     // System.out.println ("Your BMI is " + bmi);
-
     System.out.println ("Your BMI is " + bmi);
   }
 
