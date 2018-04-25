3fa6a28fb9230248e3a1f54b8aaac5bf489ec570
Adam Phair <adam.l.phair@gmail.com>
2018-04-25 12:10:45 -0400

    added all switch cases

--- ex_22/ThirtyDays/src/thirtydays/ThirtyDays.java
+++ ex_22/ThirtyDays/src/thirtydays/ThirtyDays.java
@@ -33,7 +33,43 @@ public class ThirtyDays {
                 break;
             case 4 : monthName = "April"; 
                 break;   
+            case 5 : monthName = "May"; 
+                break;
+            case 6 : monthName = "June"; 
+                break;
+            case 7 : monthName = "July"; 
+                break;
+            case 8 : monthName = "August"; 
+                break;    
+            case 9 : monthName = "September"; 
+                break;
+            case 10 : monthName = "October"; 
+                break;
+            case 11 : monthName = "November"; 
+                break;    
+            case 12 : monthName = "December"; 
+                break;    
+            default : monthName = "error";    
         }
+        
+        /*  Thirty days hath September
+            April, June, and Novemeber
+            All the rest have thirty-one
+            Except the second month alone....
+        */
+        
+        switch(month) {
+            case 9:
+            case 4:    
+            case 6:
+            case 11: days = 30;
+                     break;
+            case 2 : days = 28;
+                     break; 
+            default: days = 31;          
+        }
+        
+        System.out.println(days + " days hath " + monthName);
     }
     
 }
