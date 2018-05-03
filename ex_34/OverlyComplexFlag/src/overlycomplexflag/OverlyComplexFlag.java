3b4dd2a69b9df4164d24b7135fc56f2b01b3041f
Adam Phair <adam.l.phair@gmail.com>
2018-05-03 15:25:52 -0400

    update to ex_34

---
+++ ex_34/OverlyComplexFlag/src/overlycomplexflag/OverlyComplexFlag.java
@@ -0,0 +1,72 @@
+package overlycomplexflag;
+import static java.lang.System.*;
+/**
+ *
+ * @author adamphair
+ */
+public class OverlyComplexFlag {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        printTopHalf();
+        
+        print48Colons();
+        print48Ohs();
+        print48Colons();
+        print48Ohs();
+        print48Colons();
+        print48Ohs();        
+    }
+    public static void print48Colons() {
+        out.println("|::::::::::::::::::::::::::::::::::::::::::::::::|");
+    }
+    
+    public static void print48Ohs(){
+        out.println("|OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO|");
+    }
+    
+    public static void print29Colons() {
+        out.println("|:::::::::::::::::::::::::::::|");       
+    }
+    
+    public static void printPledge() {
+        out.println("I pledge allegiance to the flag.");
+    }
+    
+    public static void print29Ohs() {
+        out.println("|OOOOOOOOOOOOOOOOOOOOOOOOOOOOO|");
+    }
+    
+    public static void print6Stars() {
+        out.println("| *  *  *  *  *  * ");
+    }
+    
+    public static void print5Stars() {
+        out.println("| *  *  *  *  *  ");
+    }
+    
+    public static void printSixStarLine() {
+        print6Stars();
+        print29Ohs();
+    }
+    
+    public static void printFiveStarLine() {
+        print5Stars();
+        print29Colons();    
+    }
+    
+    public static void printTopHalf() {
+        out.println(" ________________________________________________ ");
+        
+        printSixStarLine();
+        printFiveStarLine();    
+        printSixStarLine();
+        printFiveStarLine();
+        printSixStarLine();
+        printFiveStarLine();
+        printSixStarLine();
+
+    }
+}
